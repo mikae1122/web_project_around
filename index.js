7625dd834e8055f0f8c37af04bb47530e687e530
mikae1122 <mikaelcorrea04@gmail.com>
2025-01-28 19:58:20 -0300

    adcionando o js

---
+++ index.js
@@ -0,0 +1,8 @@
+const popup = document.querySelector(".popup__relative");
+
+function Openbutton() {
+  popup.classList.add("popup__flex");
+}
+function Closebutton() {
+  popup.classList.remove("popup__flex");
+}
