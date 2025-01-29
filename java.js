4d2618ec4b440064de360ded9d438cca32e4783d
mikae1122 <mikaelcorrea04@gmail.com>
2025-01-28 22:54:13 -0300

    ajustando toqeus finais

--- java.js
+++
@@ -1,9 +0,0 @@
-const popup = document.querySelector(".popup__relative");
-
-function Openpopup() {
-  popup.classList.add("popup__flex");
-  console.log("1");
-}
-function ClosePopup() {
-  popup.classList.remove("popup__flex");
-}
